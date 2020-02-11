81e3b296f21cff3bebdccb9ec071cfb91914a87d
josorio <josorio@PSLT3469.personalsoft.com>
2020-02-11 16:09:33 -0500

    Se actualizan inyecciones de repositorio en service.

--- src/main/java/com/ecommerce/main/service/CategoriaServiceImpl.java
+++ src/main/java/com/ecommerce/main/service/CategoriaServiceImpl.java
@@ -2,17 +2,22 @@ package com.ecommerce.main.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ecommerce.main.entities.Categoria;
+import com.ecommerce.main.repository.ITCategoriaRepository;
 
 @Service
 public class CategoriaServiceImpl implements ITCategoriaService {
+	
+	@Autowired
+	ITCategoriaRepository repoCategoriaService;
 
 	@Override
 	public List<Categoria> listarCategoriaAll() throws Exception {
 		// TODO Auto-generated method stub
-		return null;
+		return repoCategoriaService.listarCategiraAll();
 	}
 
 }
--- src/main/java/com/ecommerce/main/service/FacturaServiceImpl.java
+++ src/main/java/com/ecommerce/main/service/FacturaServiceImpl.java
@@ -2,35 +2,39 @@ package com.ecommerce.main.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ecommerce.main.entities.Factura;
+import com.ecommerce.main.repository.ITFacturaRepository;
 
 @Service
 public class FacturaServiceImpl implements ITFacturaService{
+	
+	@Autowired
+	ITFacturaRepository repoFacturaService;
 
 	@Override
 	public void crearFactura(Factura factura) throws Exception {
-		// TODO Auto-generated method stub
+		repoFacturaService.crearFactura(factura);
 		
 	}
 
 	@Override
 	public void modificarFactura(Factura factura) throws Exception {
-		// TODO Auto-generated method stub
+		repoFacturaService.modificarFactura(factura);
 		
 	}
 
 	@Override
 	public List<Factura> listarFacturasAll() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		return repoFacturaService.listarFacturasAll();
 	}
 
 	@Override
 	public Factura getFacturaById(long factId) throws Exception {
 		// TODO Auto-generated method stub
-		return null;
+		return repoFacturaService.getFacturaById(factId);
 	}
 
 }
