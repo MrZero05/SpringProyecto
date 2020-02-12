979361fe7286a42b6177ea877b73582a00c392dc
Andres Raul Moreno Lopez <anmoreno@personalsoft.com.co>
2020-02-12 14:15:56 -0500

    implementacion consulta de productor por categorias

--- src/main/java/com/ecommerce/main/repository/ITProductosRepository.java
+++ src/main/java/com/ecommerce/main/repository/ITProductosRepository.java
@@ -9,5 +9,5 @@ public interface ITProductosRepository {
 	public void updateProducto(Producto producto)throws Exception;
 	public List<Producto> listProductoAll()throws Exception;
 	public Producto getProductoById(long prodId)throws Exception;
-	public List<Producto> listProductoByCategoria(long catId)throws Exception;
+	public List<Producto> listProductoByCategoria(String catNombre)throws Exception;
 }
